8f3ebbed6b8f1d328997756dc392a90592faf4f0
Anshu-maury <anshumaurya88979@gmail.com>
2025-01-21 19:43:31 +0530

    added document to database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const {adminAuth} = require("./middlewares/auth")
 const {connectDb} = require("./config/database")
+const User = require("./models/user")
 const app = express();
 
+    app.post("/signup",async(req,res) => {
+        // Creating a new instaance of the User Model
+        const user = new User({
+            firstName:"Muskan",
+            lastName:"Maurya",
+            emailId:"muskan@gmail.com",
+            password:"muskan123"
+        });
+        try{
+        await user.save();           //return a promise (save method)
+        res.send("User added sucessfully")
+        }
+        catch(err){
+            res.status(400).send("Error saving the user:" + err.message);
+        }
+    })
     connectDb()
     .then(() => {
         console.log("Database connected");
