bd4bcd3127c1a849efbd523838b44ba68bd83932
Anshu-maury <anshumaurya88979@gmail.com>
2025-01-21 20:25:50 +0530

    get API

--- src/app.js
+++ src/app.js
@@ -16,6 +16,27 @@ const app = express();
             res.status(400).send("Error saving the user:" + err.message);
         }
     })
+
+    // Get user by email
+    app.get("/user",async(req,res) => {
+        const userEmail = req.body.emailId;
+        try{
+        const user = await User.find({emailId:userEmail})
+        res.send(user);
+        }catch(err){
+            res.status(400).send("something went wrong")
+        }
+    })
+
+    // feed api which get all the user
+    app.get("/feed",async(req,res) => {
+        try{
+            const user = await User.find({})
+            res.send(user);
+            }catch(err){
+                res.status(400).send("something went wrong")
+            }
+    });
     connectDb()
     .then(() => {
         console.log("Database connected");
