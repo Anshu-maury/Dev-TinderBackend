f93f3c95049360c9996d5c7764ed63b1748a01f4
Anshu-maury <anshumaurya88979@gmail.com>
2025-01-22 17:39:58 +0530

    Created a jwt token

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const {adminAuth} = require("./middlewares/auth")
 const {connectDb} = require("./config/database");
 const bcrypt = require("bcrypt");
 const {validateSignUpData} = require("./utils/validation")
-const User = require("./models/user")
+const User = require("./models/user");
+const cookieParser = require("cookie-parser");
+const jwt =require("jsonwebtoken");
 const app = express();
      app.use(express.json());   //its a middleware which reads the json object and converts it into js object and it add that object to the req body
-    app.post("/signup",async(req,res) => {
+     app.use(cookieParser());
+     app.post("/signup",async(req,res) => {
         try{
             // validation of data
             validateSignUpData(req);
@@ -27,7 +30,51 @@ const app = express();
             res.status(400).send("Error saving the user:" + err.message);
         }
     })
+    app.post("/login",async(req,res) =>{
+        try{
+        const {emailId,password}= req.body;
+        const user = await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("Invalid Credential");
+        }
+        const ispassowrdValid= await bcrypt.compare(password,user.password);
+        if(ispassowrdValid){
+            // Create a JWT token
+            const token = await jwt.sign({_id:user._id},"Muskan@20")
+            // Add the token to cookie and send the repsonse back to the user
+            res.cookie("token",token)    //saving the cookie
+            res.send("login sucessfully")
+        }
+        else{
+            res.send("Invalid Credential")
+        }
+    }catch(err){
+        res.status(400).send("ERROR:" + err.message)
+    }
+    })
 
+    app.get("/profile",async(req,res) => {
+        try{
+        const cookies = req.cookies;
+        const {token} = cookies;
+        if(!token){
+            throw new Error("Invalid Token")
+        }
+
+        // Validate the token
+        const decodedMessage = await jwt.verify(token,"Muskan@20");
+        const {_id} = decodedMessage;
+        console.log("Logged in user is "+ _id);
+        const user =await User.findById({_id});
+        if(!user){
+            throw new Error("User not found")
+        }
+        res.send(user)
+    }
+    catch(err){
+        res.status(400).send("Error: " + err.message)
+    }
+    })
     // Get user by email
     app.get("/user",async(req,res) => {
         const userEmail = req.body.emailId;
